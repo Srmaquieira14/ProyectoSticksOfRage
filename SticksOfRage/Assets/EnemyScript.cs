2a5d1cd9847e8a9825c42ed559bb298d5e84105a
Srmaquieira14 <Srmaquieira14@gmail.com>
2021-11-10 21:06:04 +0100

    Commit con pequeños avances
    Empezando a intentar mover sprites por pantalla y definir su posición incial

---
+++ SticksOfRage/Assets/EnemyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Rigidbody2D enemyRb;
+    public float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.localPosition.x, 0.0F, 0.0F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
+}
---
+++ SticksOfRage/Assets/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.localPosition.x, 0.0F, 0.0F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
